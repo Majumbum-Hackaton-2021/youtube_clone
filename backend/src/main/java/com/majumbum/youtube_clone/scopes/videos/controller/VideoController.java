d7a137fed30754ac4fc3e6ad4ccd45ec627cfd44
michael-mb <mbonimichael@yahoo.fr>
2021-09-09 15:41:09 +0200

    videos

---
+++ backend/src/main/java/com/majumbum/youtube_clone/scopes/videos/controller/VideoController.java
@@ -0,0 +1,10 @@
+package com.majumbum.youtube_clone.scopes.videos.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/videos")
+public class VideoController {
+
+}
