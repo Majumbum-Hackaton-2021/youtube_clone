2f08ef5032b394538a31db1b3d9d0f39648f982f
michael-mb <mbonimichael@yahoo.fr>
2021-09-09 15:31:34 +0200

    file upload work

---
+++ backend/src/main/java/com/majumbum/youtube_clone/scopes/videos/storage/FileUploadController.java
@@ -0,0 +1,34 @@
+package com.majumbum.youtube_clone.scopes.videos.storage;
+
+import com.majumbum.youtube_clone.scopes.auth.payload.response.MessageResponse;
+import com.majumbum.youtube_clone.scopes.videos.storage.exceptions.StorageFileNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/upload")
+public class FileUploadController {
+
+    private final StorageService storageService;
+
+    public FileUploadController(StorageService storageService) {
+        this.storageService = storageService;
+    }
+
+
+    @PostMapping("/")
+    public ResponseEntity<?> handleFileUpload(@RequestBody MultipartFile file) {
+
+        if(file == null)
+            return ResponseEntity.ok(new MessageResponse("The file should not be null"));
+        storageService.store(file);
+        return ResponseEntity.ok(new MessageResponse("File successfully uploaded"));
+    }
+
+    @ExceptionHandler(StorageFileNotFoundException.class)
+    public ResponseEntity<?> handleStorageFileNotFound(StorageFileNotFoundException exc) {
+        return ResponseEntity.notFound().build();
+    }
+
+}
