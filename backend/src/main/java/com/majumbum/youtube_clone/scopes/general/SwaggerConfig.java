6dd6bad83cf2187dc7c3f73057a4cabf68c04685
michael-mb <mbonimichael@yahoo.fr>
2021-09-25 06:54:55 +0200

    add swagger

---
+++ backend/src/main/java/com/majumbum/youtube_clone/scopes/general/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package com.majumbum.youtube_clone.scopes.general;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.majumbum.youtube_clone.scopes"))
+                .paths(PathSelectors.any())
+                .build().apiInfo(metaData());
+    }
+
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder()
+                .title("Youtube Clone")
+                .description("\"Rest API Documentation \"")
+                .version("1.1.0")
+                .license("Apache 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
+                .contact(new Contact("Michael Mboni", "https://michael-mb.github.io/Michael-CV/", "mbonimichael@yahoo.fr"))
+                .build();
+    }
+}
